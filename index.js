64fc13630a06a554d12dc21ff6a54174bfc3cfc6
dowdje <dowdje89@gmail.com>
2016-09-26 08:29:27 -0400

    Built a Component with props

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img src="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="Mad Max: Fury Road" />
+        <h2>Mad Max: Fury Road</h2>
+        <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
+      </div>
+    );
+  }
+}
+
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
